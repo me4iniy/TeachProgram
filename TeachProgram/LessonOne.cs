a6fa3e1eed498b452d753784abdad8f8f110fd87
Kirill <me4iniy@gmail.com>
2023-07-29 18:08:14 +0500

    New lesson

---
+++ TeachProgram/LessonOne.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeachProgram
+{
+    public class LessonOne
+    {
+        /*
+         * 1) Создать одну переменную, что выведет четное число, дробное, логическое.
+         * 
+         * 2) Вывести число по формуле x * 1.2, только целочисленное (от 1 до 10000)
+         * 
+         * 3) Вывесли от 1 до 10000 пропуская все кратные 3
+         * 
+         * 4) Sum(2, 9) 
+         * 
+         * 5) ('+', 4, 7) --> 11
+         * 
+         * 6) "aa", "bb" , "cc" => aabbcc
+         * 
+         * 7) Исключить все нули из числа
+         * 
+         * 8) https://www.codewars.com/kata/57e92e91b63b6cbac20001e5 Сложное!
+         * 
+         * 9) https://www.codewars.com/kata/588417e576933b0ec9000045 Сложное!
+         */
+    }
+}
